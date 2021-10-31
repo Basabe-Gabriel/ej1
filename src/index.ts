75e9c0c372437566ab4e6ced2a329be4c2f19435
Basabe-Gabriel <92063962+Basabe-Gabriel@users.noreply.github.com>
2021-10-31 08:16:39 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+let n1: number = 0;
+let contador: number = 1;
+let suma: number = 0;
+
+while (contador <= 5) {
+  //n1 = Number (prompt("ingrese el numero"));
+  if (n1 < 1 && n1 > 10) {
+    suma = n1 + 1;
+    contador = contador + 1;
+  }
+}
